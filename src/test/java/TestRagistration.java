7c157da4ff5815b29efe8cfefb7a437d57ab73b1
shitalSonwane <shitals177@gmail.com>
2020-03-22 16:24:50 +0530

    Added:CheckFirstName

--- src/test/java/TestRagistration.java
+++ src/test/java/TestRagistration.java
@@ -1,2 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestRagistration {
+    Ragistraion obj=new Ragistraion();
+    @Test
+    public void givenfirstName_whenValid_thenTrue() {
+        boolean firstname=obj.checkName("Ganga");
+        Assert.assertTrue(firstname);
+    }
+
+    @Test
+    public void givenfirstName_whenInvalid_thenFalse() {
+        boolean firstname=obj.checkName("ganga");
+        Assert.assertFalse(firstname);
+    }
 }
