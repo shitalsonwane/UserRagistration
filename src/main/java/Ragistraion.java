5dd87b3f0979d5194146d9e492c9acdc43d01d0a
shitalSonwane <shitals177@gmail.com>
2020-03-22 20:36:49 +0530

    Added:Password with Cap

--- src/main/java/Ragistraion.java
+++ src/main/java/Ragistraion.java
@@ -5,6 +5,7 @@ public class Ragistraion {
     String pattern2="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
     String pattern3="[0-9]{2}[ ]{1}[0-9]{10}$";
     String pattern4="[a-zA-Z0-9]{8,}$";
+    String pattern5="^([a-zA-Z0-9]*[A-Z]+[a-zA-Z0-9]*)+$";
     public static void main(String args[])
     {
         System.out.println("---Welcome to User Ragistration---");
@@ -18,7 +19,16 @@ public class Ragistraion {
     public boolean checkMobileNumber(String number) {
         return Pattern.matches(pattern3,number);
     }
-    public boolean checkPassword(String pass) {
+    public boolean checkPassword1(String pass) {
         return Pattern.matches(pattern4,pass);
+    }
+
+    public boolean checkPassword2(String pass) {
+        if(pass.length()>=8) {
+            return (Pattern.matches(pattern5, pass));
+        }
+        else {
+            return false;
+        }
     }
 }
