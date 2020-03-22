b27b2e47d7d886b12b1d1785047e6f39e3f13085
shitalSonwane <shitals177@gmail.com>
2020-03-22 16:44:54 +0530

    Added:CheckEmail

--- src/main/java/Ragistraion.java
+++ src/main/java/Ragistraion.java
@@ -2,11 +2,15 @@ import java.util.regex.Pattern;
 
 public class Ragistraion {
     String pattern1="^[A-Z][a-zA-Z]{2,}$";
+    String pattern2="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
     public static void main(String args[])
     {
         System.out.println("---Welcome to User Ragistration---");
     }
     public boolean checkName(String username) {
         return Pattern.matches(pattern1,username);
+    }
+    public boolean checkEmail(String email) {
+        return Pattern.matches(pattern2,email);
     }
 }
