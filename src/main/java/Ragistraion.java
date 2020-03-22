6f75c64236ce3259105235a1317f33a44bcc46eb
shitalSonwane <shitals177@gmail.com>
2020-03-22 16:23:53 +0530

    Added:CheckFirstName

--- src/main/java/Ragistraion.java
+++ src/main/java/Ragistraion.java
@@ -1,6 +1,12 @@
+import java.util.regex.Pattern;
+
 public class Ragistraion {
+    String pattern1="^[A-Z][a-zA-Z]{2,}$";
     public static void main(String args[])
     {
         System.out.println("---Welcome to User Ragistration---");
+    }
+    public boolean checkName(String fname) {
+        return Pattern.matches(pattern1,fname);
     }
 }
