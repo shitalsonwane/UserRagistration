1b156b79e0f9e806d96d6efd6fd3ecebd04a14d1
shitalSonwane <shitals177@gmail.com>
2020-03-22 16:49:48 +0530

    Added:CheckMobileNumber

--- src/main/java/Ragistraion.java
+++ src/main/java/Ragistraion.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 public class Ragistraion {
     String pattern1="^[A-Z][a-zA-Z]{2,}$";
     String pattern2="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
+    String pattern3="[0-9]{2}[ ]{1}[0-9]{10}$";
     public static void main(String args[])
     {
         System.out.println("---Welcome to User Ragistration---");
@@ -12,5 +13,8 @@ public class Ragistraion {
     }
     public boolean checkEmail(String email) {
         return Pattern.matches(pattern2,email);
+    }
+    public boolean checkMobileNumber(String number) {
+        return Pattern.matches(pattern3,number);
     }
 }
