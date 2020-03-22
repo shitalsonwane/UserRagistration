34f247fc2e3abb711ef09bf9456c67c161b410fe
shitalSonwane <shitals177@gmail.com>
2020-03-22 20:25:40 +0530

    Added:Password With At least eight char

--- src/main/java/Ragistraion.java
+++ src/main/java/Ragistraion.java
@@ -4,6 +4,7 @@ public class Ragistraion {
     String pattern1="^[A-Z][a-zA-Z]{2,}$";
     String pattern2="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$";
     String pattern3="[0-9]{2}[ ]{1}[0-9]{10}$";
+    String pattern4="[a-zA-Z0-9]{8,}$";
     public static void main(String args[])
     {
         System.out.println("---Welcome to User Ragistration---");
@@ -16,5 +17,8 @@ public class Ragistraion {
     }
     public boolean checkMobileNumber(String number) {
         return Pattern.matches(pattern3,number);
+    }
+    public boolean checkPassword(String pass) {
+        return Pattern.matches(pattern4,pass);
     }
 }
